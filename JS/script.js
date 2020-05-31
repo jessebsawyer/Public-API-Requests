79b25680da1c1210f48b86c57ff4cf3ec5ec0658
jessebsawyer <46061645+jessebsawyer@users.noreply.github.com>
2020-05-31 17:33:47 -0400

    update

--- JS/script.js
+++ JS/script.js
@@ -2,13 +2,13 @@
 const url = 'https://randomuser.me/api/?results=12';
 const gallery = document.getElementById('gallery');
 const searchDiv = document.querySelector('.search-container');
-
+let current = 1;
 
 // Create Search Bar
 searchDiv.innerHTML = 
 `<form action="#" method="get">
-<input type="search" id="search-input" class="search-input" placeholder="Search...">
-<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
 </form>`;
 
 // Grab ID's of Search Input
@@ -25,14 +25,15 @@ function getApi(url) {
 getApi(url)
 .then(data => {
     let people = data.results;
-    people.map(person => {
-        galleryHtml(person);
-    })
+    people.map(person => galleryHtml(person))
+    people.map(person => modalHtml(person))
 })
+
 
 // Event Listeners
 searchSubmit.addEventListener('click', searchButton);
 searchSpace.addEventListener('keyup', searchInput);
+document.addEventListener('click', showVaribales);
 
 
 // Helper Functions:
@@ -40,115 +41,87 @@ searchSpace.addEventListener('keyup', searchInput);
 // Display Gallery With Data From API
 function galleryHtml(data) {
     this.data = data;
-   const div = document.createElement('div');
-   const img = document.createElement('img');
-   const divCard = document.createElement('div');
-   const divInfo = document.createElement('div');
-   const h3 = document.createElement('h3');
-   const email = document.createElement('p');
-   const location = document.createElement('p');
-   div.className = 'card';
-   divCard.className = 'card-img-container';
-   img.className = 'card-img';
-   img.src = data.picture.medium;
-   img.alt = 'Profile Picture';
-   divInfo.className = 'card-info-container';
-   h3.id = 'name';
-   h3.className = 'card-name';
-   h3.className = 'cap';
-   h3.textContent = `${data.name.first}, ${data.name.last}`;
-   email.className = 'card-text';
-   email.textContent = data.email;
-   location.className = 'card-text';
-   location.className = 'cap';
-   location.textContent = `${data.location.city}, ${data.location.state}`;
-   div.appendChild(divCard);
-   div.appendChild(divInfo);
-   divCard.appendChild(img);
-   divInfo.appendChild(h3);
-   divInfo.appendChild(email);
-   divInfo.appendChild(location);
-   gallery.appendChild(div);
-   modalHtml(this.data, div);
+    const div = document.createElement('div');
+    div.className = 'card';
+    div.innerHTML = ` <div class="card-img-container">
+    <img class="card-img" src="${data.picture.medium}" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+        <p class="card-text">${data.email}</p>
+        <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+    </div>`;
+    gallery.appendChild(div);
+    // modalHtml(this.data, div);
+    
 }
 
 // Display Modal of Employee 
 function modalHtml(data, div) {
-    div.addEventListener('click', (e) => {
-        console.log(data);
+    // div.addEventListener('click', (e) => {
+        // console.log(data);
         const modalContainer = document.createElement('div');
-        console.log(modalContainer)
-        const modalBtnContainer = document.createElement('div');
-        const modal = document.createElement('div');
-        const x = document.createElement('btn');
-        const btnRight = document.createElement('btn');
-        const btnLeft = document.createElement('btn');
-        const modalInfoContainer = document.createElement('div');
-        const img = document.createElement('img');
-        const h3 = document.createElement('h3');
-        const email = document.createElement('p');
-        const city = document.createElement('p');
-        const hr = document.createElement('hr');
-        const phone = document.createElement('p');
-        const address = document.createElement('p');
-        const birth = document.createElement('p');
         modalContainer.className = 'modal-container';
-        modalBtnContainer.className = 'modal-btn-container'
-        modal.className = 'modal';
-        modalInfoContainer.className = 'modal-info-container';
-        x.innerHTML = ` <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
-        img.className = 'modal-img';
-        img.src = data.picture.large;
-        img.alt = 'profile picture';
-        h3.id = 'name';
-        h3.className = 'modal-name';
-        h3.textContent = `${data.name.first} ${data.name.last}`;
-        h3.className = 'cap';
-        email.className = 'modal-text';
-        email.textContent = data.email;
-        city.className = 'modal-text';
-        city.textContent = data.location.city
-        phone.className = 'modal-text';
-        phone.textContent = data.phone;
-        address.className = 'modal-text';
-        address.textContent = `${data.location.street.number} ${data.location.street.name}, ${data.location.city}, ${data.location.state}, ${data.location.postcode}`;
-        birth.className = 'modal-text';
-        birth.textContent = `Birthday: ${data.dob.date.slice(0,10)}`;
-        btnRight.className = 'modal-next';
-        btnRight.className = 'btn';
-        btnRight.id = 'modal-next';
-        btnRight.textContent = 'Next';
-        btnRight.type = 'button';
-        btnLeft.className = 'modal-prev';
-        btnLeft.className = 'btn'
-        btnLeft.id = 'modal-prev';
-        btnLeft.type = 'button';
-        btnLeft.textContent = 'Prev';
-        modalContainer.appendChild(modal);
-        modalContainer.appendChild(modalBtnContainer);
-        modalBtnContainer.appendChild(btnLeft);
-        modalBtnContainer.appendChild(btnRight);
-        modal.appendChild(x);
-        modal.appendChild(modalInfoContainer);
-        city.appendChild(hr);
-        modalInfoContainer.appendChild(img);
-        modalInfoContainer.appendChild(h3);
-        modalInfoContainer.appendChild(email);
-        modalInfoContainer.appendChild(city);
-        modalInfoContainer.appendChild(phone);
-        modalInfoContainer.appendChild(address);
-        modalInfoContainer.appendChild(birth);
-        div.appendChild(modalContainer);
-    });
+        modalContainer.innerHTML = 
+        `<div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${data.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+                <p class="modal-text">${data.email}</p>
+                <p class="modal-text cap">${data.location.city}</p>
+                <hr>
+                <p class="modal-text">${data.phone}</p>
+                <p class="modal-text">${data.location.street.number} ${data.location.street.name}, ${data.location.city}, ${data.location.state}, ${data.location.postcode}</p>
+                <p class="modal-text">Birthday: ${data.dob.date.slice(0,10)}</p>
+            </div>
+        </div>
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>`;
+        gallery.appendChild(modalContainer);
+        modalContainer.style.display = 'none';
+        function showModal (div) {
+            div.addEventListener('click', modalContainer.style.display = '');
+        }
+        const x = document.getElementById('modal-close-btn');
+        // x.addEventListener('click', () => modalContainer.remove());
+        
+    // });
 }
 
-function removeModal () {
-    const x = document.querySelector('.modal-close-btn');
-    const modalContainer = document.querySelector('.modal-container');
-    x.addEventListener('click', () => modalContainer.remove());
+// Variables 
+const x = document.getElementById('modal-close-btn');
+const modalContainer = document.querySelectorAll('.modal-container');
+const cardDiv = document.querySelectorAll('.card');
+
+// // Scroll Through Modal
+// function setModal(modals) {
+//     modals[0].id = '0';
+//     modals[1].id = '1';
+//     modals[2].id = '2';
+//     modals[3].id = '3';
+//     modals[4].id = '4';
+//     modals[5].id = '5';
+//     modals[6].id = '6';
+//     modals[7].id = '7';
+//     modals[8].id = '8';
+//     modals[9].id = '9';
+//     modals[10].id = '10';
+//     modals[11].id = '11';
+//     console.log(modals);
+// }
+
+function showVaribales() {
+    const cardDiv = document.querySelectorAll('.card');
+    const x = document.getElementById('modal-close-btn');
+    console.log(cardDiv,x);
 }
 
-removeModal();
+const card = document.querySelectorAll('.card');
+const xBtn = document.getElementById('modal-close-btn');
+console.log(card, xBtn);
 
 // Search Employee via Button
 function searchButton(e) {
@@ -157,10 +130,8 @@ function searchButton(e) {
     div.forEach(person => {
         let a = person.querySelector('h3');
         if (a.innerHTML.toUpperCase().indexOf(searchValue) > -1) {
-            console.log('matched with...', a);
             a.parentElement.parentElement.style.display = '';
         }else {
-            console.log('No match');
             a.parentElement.parentElement.style.display = 'none';
         }
     })
@@ -174,10 +145,8 @@ function searchInput() {
     div.forEach(person => {
         let a = person.querySelector('h3');
         if (a.innerHTML.toUpperCase().includes(searchValue) ) {
-            console.log('matched with...', a);
             a.parentElement.parentElement.style.display = '';
         }else {
-            console.log('No match');
             a.parentElement.parentElement.style.display = 'none';
         }
     })
